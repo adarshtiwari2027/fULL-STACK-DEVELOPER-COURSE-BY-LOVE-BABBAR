607625cc21326a07b90f00f00e5c363fe15997ac
adarshtiwari2027 <adarshtiwarq2323@gmail.com>
2025-01-18 12:27:11 +0530

    till operator javascript fiinish

---
+++ JAVASCRIPT/JAVASRIPT01/index.js
@@ -0,0 +1,28 @@
+console.log('hello word my name is adarsh tiwari')
+let a = 'adarsh';
+console.log(a);
+var adarsh = 3;
+var adarsh = 7;
+console.log(adarsh);
+let age = 26;
+let status1 = (age >= 18) ? 'vote' : 'no vote';
+console.log(status1);
+
+let vikash = (1 && false);
+console.log(vikash);
+let marks = 70;
+if (marks >= 90) {
+    console.log('excellent');
+
+} else if (marks >= 80) {
+    console.log('good ');
+
+} else if (marks < 30) {
+    console.log('fail');
+} else {
+    console.log('nice');
+}
+for (let i = 0; i < 5; i++) {
+    console.log('adarsh')
+
+}
