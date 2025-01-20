52635c32400b5d53dc47634e2e9f39a571e17f42
adarshtiwari2027 <adarshtiwarq2323@gmail.com>
2025-01-20 11:50:14 +0530

    function completed ...

--- JAVASCRIPT/javascript02/index.js
+++ JAVASCRIPT/javascript02/index.js
@@ -1,9 +1,38 @@
-console.log('hello ji')
-    // object creation 
-const rectangle = {
-    length: 1,
-    breadth: 2,
-    draw: function() {
-        console.log('draw rectangle');
+// console.log('hello ji')
+//     // object creation 
+
+
+// //factory function
+
+// function createRectangle(len, bre) {
+
+
+//     let rectangle = {
+//         length: len,
+//         breadth: bre,
+//         draw: function() {
+//             console.log('draw rectangle');
+//         }
+//     };
+//     return rectangle;
+// }
+
+// let object1 = createRectangle(5, 4);
+// let object2 = createRectangle(3, 6);
+// constructer function ''' pastcle notation in which firct word of every key is capital
+function Rectangle(len, bre, ) {
+    this.length = len;
+    this.breadth = bre;
+    this.draw = function() {
+        console.log('drawing')
     }
-};
+}
+
+//object created using constructur..
+let rectangleobject = new Rectangle(4, 6, );
+// add property later if function is created ...
+rectangleobject.height = 5;
+console.log(rectangleobject);
+//delete after function is creted
+delete rectangleobject.length;
+console.log(rectangleobject);
